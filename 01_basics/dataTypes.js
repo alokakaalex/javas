80da1f6d98358a940dd34841b8bac4f35607c91c
alokakaalex <iamalokmishra242@gmail.com>
2025-07-17 23:53:46 +0000

    discussed dataypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // use newer version of js
+
+//alert(3+2)// used to give pop ups but in the browser, different syntax for this in node
+
+let name = "hitesh"; //strings
+let age = 15; //int
+let isLoggedIn = false; //bool
+
+// number => 2 to power 53
+// bigint
+//string=>""
+//boolean=>true/false
+//null => standalone value
+// undefined => value not assigned
+// symbol => to find uniqueness
+
+
+//object
+
+console.log(typeof "hitesh"); //typeof gives the types of varibales
+console.log(typeof age);
+
+console.log(typeof undefined);
+console.log(typeof null); // it belongs to the object
+
+
