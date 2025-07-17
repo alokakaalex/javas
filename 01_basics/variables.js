ffbde6f8520d6a7e2b22c329d0ae1c80c1f8c5ec
alokakaalex <iamalokmishra242@gmail.com>
2025-07-17 23:33:54 +0000

    ;earnt about variables and constants

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = "144553";
+let accountEmail = "hitesh@gmail.com";
+var accountPassword = "12345";
+accountCity = "Delhi";
+
+// accountId=2; not allowed
+accountEmail="hc@hc.com";
+accountPassword="2121212";
+accountCity="bengaluru";
+/*
+prefer not to use var beacuse of problem in blocks
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+let accountState; //by default undefined will be assigned to this variable
