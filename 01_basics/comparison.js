ed3a0d77f4ad4c729cc22ec9d7d2eb99ac0f294d
alokakaalex <iamalokmishra242@gmail.com>
2025-07-18 00:43:22 +0000

    comparisons done

---
+++ 01_basics/comparison.js
@@ -0,0 +1,18 @@
+//simple comparisons
+/*console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);*/
+
+//complex comparisons
+/*console.log("2">1); //they might give unexpected results
+console.log("02">1); */ //hence it is advised to use sme datatypes for comparisons
+
+
+/*console.log(null >0);// equality and comparisons operator works differently
+
+console.log(null == 0); // comparison operator converts null to 0 and then compare
+console.log(null>=0);*/
+
+// strict check ===
+//checks value as well as datatypes
