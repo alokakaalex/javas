9d5cfd788bf3fbf1c5686664d5453e0deeafde9c
alokakaalex <iamalokmishra242@gmail.com>
2025-07-18 00:32:04 +0000

    convertions and operations done

--- 01_basics/conversion_operations.js
+++ 01_basics/conversion_operations.js
@@ -1,14 +1,30 @@
 let score = 33;
 
-console.log(typeof score); // can be used normally
-console.log(typeof(score)); // can be used as method as well
+//console.log(typeof score); // can be used normally
+//console.log(typeof(score)); // can be used as method as well
 
 let number = "56";
 let valueInNumber = Number(number); //it will convert anything to number
-console.log(valueInNumber); 
+//console.log(valueInNumber); 
 
 // "33"=> 33
 // "33abc" => will get convert but value will be NaN
 // true=>1
 
 // 1->true, 0=>false, ""=> false, "hitesh"=> true
+
+// *********************************Operations *************************//
+let value = 3;
+let neg = -value;
+/*console.log(neg);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);*/
+
+/*console.log("1"+2); // it acts as strings
+console.log(1+"2");// it also acts as strings
+console.log("1"+2+3);//it also acts as strings
+console.log(1+2+"2"); // here first 1+2 happens and then 2 is added to the string*/
