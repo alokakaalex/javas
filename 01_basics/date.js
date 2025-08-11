2fea7b5601529be720fad73865abb8e632f69de5
alokakaalex <iamalokmishra242@gmail.com>
2025-08-11 15:10:49 +0000

    some more additional changes

---
+++ 01_basics/date.js
@@ -0,0 +1,16 @@
+const mydate = new Date();
+console.log(mydate);
+console.log(mydate.toString());
+console.log(mydate.toLocaleDateString());
+
+console.log(typeof mydate); // date is ot the object type
+const newDate = new Date(2023,2,18); // while declaring datenotice that months start from 0
+console.log(newDate.toDateString());
+
+const date1= new Date("03-18-2023"); // usually we use this in india mm-dd-yyyy
+console.log(date1);
+
+let mytime= Date.now(); // it will give value in miliseconds from january 1970
+console.log(mytime);
+console.log(date1.getTime()); // in this way we can compare two given time usually we use this in hotel booking system
+// to convert the time in sec we can divide it by 1000 to get result in seconds
