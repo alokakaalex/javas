e4dfe96ad58d1d37f04cf16260487d11a6098582
alokakaalex <iamalokmishra242@gmail.com>
2025-08-12 05:07:11 +0000

    non-primitve

--- 02_basics/arrays.js
+++ 02_basics/arrays.js
@@ -12,3 +12,31 @@ const newArr = myArr.join() // it will simply add the array to the new array and
  const arr2 = myArr.splice(1,3); // splice will move out the portion of array between start and end including both thereby making chances in the original array also
  console.log(arr1);
  console.log(arr2);
+
+//  ++++++++++++++++++++++++++++++++++Arrays part2 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+const marvelHeroes=["thor","ironman","spiderman"];
+const dcHeroes=["superman","flash","batman"];
+
+//marvelHeroes.push(dcHeroes); // it will push the entire dcHeroes array to marvelHeroes
+//console.log(marvelHeroes);
+
+//another method
+const allHeroes = marvelHeroes.concat(dcHeroes); // it will add elements of both the arrays into a new array
+//console.log(allHeroes);
+
+//another method
+const all_heroes=[...marvelHeroes,...dcHeroes]; //... is callled as spread operator
+//console.log(all_heroes);
+
+const another_arr=[1,2,3,[4,5,6],7,[6,7,[4,5]]];
+const real_array= another_arr.flat(Infinity); // flat is used to give such complex array to a simpler form
+//console.log(real_array);
+
+console.log(Array.from("Alok")); //this method will convert anything to array
+console.log(Array.from({name:"hitesh"})); // here it will return empty array because we have to specify whether we want to convert key or valur to array
+
+let score1=100;
+let score2=200;
+let score3=300;
+console.log(Array.of(score1,score2,score3)); // it will make array of multiple different varibales
+
